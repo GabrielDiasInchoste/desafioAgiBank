8f39962674a757a7a4baa760aa9c69c8ec6d77d5
Gabriel Dias <gabriel.dias97@hotmail.com>
2020-08-22 10:58:21 -0300

    GD 01 Criando Projeto

---
+++ src/main/java/br/com/gabrielDias/desafioAgiBank/DesafioAgiBankApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrielDias.desafioAgiBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAgiBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAgiBankApplication.class, args);
+	}
+
+}
